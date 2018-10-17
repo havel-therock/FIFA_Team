7d0c1aec3ccab8a0e6ff4a941343ae264d92a55d
Mateusz Laskowski <mati1003.ml@gmail.com>
2018-10-17 23:26:23 +0200

    Czas wykonywania jest naprawdę długa do nawet 10 min!!!

--- SEM_V/Obliczenia naukowe/Lista_1/zad4_ML.jl
+++ SEM_V/Obliczenia naukowe/Lista_1/zad4_ML.jl
@@ -6,16 +6,17 @@
 
 # Podpunkt A
 println("Podpunkt A");
-#=
+
 x = Float64(1.0);
 eta = nextfloat(Float64(1.0)) - Float64(1.0);
 while (Float64(x * Float64(1.0 / x)) == 1.0 && Float64(x) < 2)
     x = x + eta;
 end
 println("x = ", x);                                             # x = 1.000000057228997
-=#
+
 # Podpunkt B
 println("Podpunkt B");
+
 x = realmin(Float64);
 while (Float64(x * Float64(1.0 / x)) == 1.0)
     x = Float64(x + (nextfloat(x) - Float64(x)));
